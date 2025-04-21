82a3303da1b7ced1e6445ec308705e438a5b3821
Nikita Danilov <danilovn1@mail.ru>
2025-04-21 20:40:11 +0500

    add sort_by_date func

--- src/processing.py
+++ src/processing.py
@@ -1,2 +1,6 @@
 def filter_by_state(data, state='EXECUTED'):
     return [item for item in data if item.get('state') == state]
+
+
+def sort_by_date(data, descending=True):
+    return sorted(data, key=lambda x: x['date'], reverse=descending)
