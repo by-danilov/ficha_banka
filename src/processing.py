27b09c28c1ab196095e3e4b52ed30e5815c1dedb
Nikita Danilov <danilovn1@mail.ru>
2025-04-22 21:40:42 +0500

    README changes

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,8 @@
-def filter_by_state(data, state='EXECUTED'):
+def filter_by_state(data: list, state: str = 'EXECUTED') -> list:
+    """Принимает список словарей с банковскими операциями и фильтрует по значению 'state'."""
     return [item for item in data if item.get('state') == state]
 
 
-def sort_by_date(data, descending=True):
-    return sorted(data, key=lambda x: x['date'], reverse=descending)
+def sort_by_date(data: list, descending: bool = True) -> list:
+    """Принимает список словарей с операциями и сортирует по ключу 'date'."""
+    return sorted(data, key=lambda item: item.get('date', ''), reverse=descending)
